189c5a03b2058ae7b6e5a0e72c99e7541a9dea94
SvetlanaOs <osa905@gmail.com>
2025-08-07 13:24:14 +0300

    Update main.py

--- main.py
+++ main.py
@@ -6,10 +6,7 @@ import re
 with open("phonebook_raw.csv", encoding="utf-8") as f:
   rows = csv.reader(f, delimiter=",")
   contacts_list = list(rows)
-#pprint(contacts_list)
-#print('jjjjjjjjjjjjjjjjjjjjjjjjjj')
 
-# TODO 1: выполните пункты 1-3 ДЗ
 for i in range (1,len(contacts_list)):
   fio = contacts_list[i][0] + ' '+ contacts_list[i][1] + ' ' + contacts_list[i][2]
   fio_list = fio.split()
@@ -42,13 +39,8 @@ for contact in contacts_list[1:]:
       if value and not existing_contact[i + 2]:
         existing_contact[i + 2] = value
 
-# Преобразование словаря обратно в список
 contacts_list = [header] + list(contacts_dict.values())
 
-print(contacts_list)
-
-# TODO 2: сохраните получившиеся данные в другой файл
-# код для записи файла в формате CSV
 with open("phonebook.csv", "w", encoding="utf-8") as f:
   datawriter = csv.writer(f, delimiter=',')
   # Вместо contacts_list подставьте свой список
